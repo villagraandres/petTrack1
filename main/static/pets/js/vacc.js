e4fb26536d58c9c5399ee56956c79958b1d14667
Andrés Nehuel <villagraandres00@gmail.com>
2023-10-02 00:06:25 -0600

    changes in styles of vaccines

--- main/static/pets/js/vacc.js
+++ main/static/pets/js/vacc.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded',()=>{
     document.querySelector('#vaccSubmit').addEventListener('click',add);
+    const exampleModal = document.getElementById('exampleModal');
+    if(exampleModal){ exampleModal.addEventListener('show.bs.modal',edit)}
+   
 })
 
 
@@ -35,4 +38,20 @@ function add(e){
         window.location.reload();
     })
     console.log('success');
+}
+
+function edit(e){
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+
 }
