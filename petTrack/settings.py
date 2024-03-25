23e52f073666e0086a8cfa79cb31eea9f39d920e
andres <villagraandres00@gmail.com>
2024-03-24 20:55:28 -0600

    err5

--- petTrack/settings.py
+++ petTrack/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'petTrack/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
