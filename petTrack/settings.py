6dc359b4ed4d406196f22306e76d7e4be54d5bc5
andres <villagraandres00@gmail.com>
2024-03-24 18:39:06 -0600

    branch

--- petTrack/settings.py
+++ petTrack/settings.py
@@ -122,7 +122,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pettrackapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'hlrhlvmxqgmekxlj'
+EMAIL_HOST_PASSWORD = 'osfx mdfv fjte cjbq'
 EMAIL_PORT = 587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
